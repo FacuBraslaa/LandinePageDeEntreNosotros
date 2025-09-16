bc4998e4f84789a25d5425ccb9bb3bf641d14e3a
FacuBraslaa <facundobraslavsky@gmail.com>
2025-09-16 15:30:19 -0300

    coomiiit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-import { Routes, Route } from 'react-router-dom'
-import { ThemeProvider } from '@/components/theme-provider'
-import { Toaster } from '@/components/ui/toaster'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "./components/theme-provider";
+import { Toaster } from "./components/ui/toaster";
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <main className="min-h-screen bg-background">
-        <Routes>
-          <Route path="/" element={<div>Bienvenido a Entre Nosotros</div>} />
-        </Routes>
-      </main>
+    <ThemeProvider attribute="class" defaultTheme="light" storageKey="vite-ui-theme">
+      <BrowserRouter>
+        {/* Si no usás shadcn/tailwind con tokens, podés cambiar bg-background por bg-white */}
+        <main className="min-h-screen bg-background">
+          <Routes>
+            <Route path="/" element={<div>Bienvenido a Entre Nosotros</div>} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+
       <Toaster />
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
